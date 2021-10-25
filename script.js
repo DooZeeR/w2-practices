24948f36820d72b7ad045d5df1a0c4fad7f07916
DooZeeR <zsolt.ordas@gmail.com>
2021-10-25 16:08:44 +0200

    Menu BTN working

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+function menu(event) {
+    event.target.classList.toggle("clicked");
+}
+
 function loadEvent() {
     const rootElement = document.getElementById('root');
+
+    document.getElementById("menu-btn").addEventListener("click", menu);
 }
-  
+
 window.addEventListener("load", loadEvent);
